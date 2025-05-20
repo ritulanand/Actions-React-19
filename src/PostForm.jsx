f6c53e320fa540c4d926f60c8ac0b8841d722dc9
ritulanand <ritulanand007@gmail.com>
2025-05-20 17:32:29 +0530

    useFormStatus hook added

--- src/PostForm.jsx
+++ src/PostForm.jsx
@@ -1,4 +1,5 @@
 import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 // import { useState, useTransition } from "react";
 
 async function submitPost(prevState, formData){
@@ -18,9 +19,30 @@ async function submitPost(prevState, formData){
   }
 }
 
+function SubmitFormBtn(){
+    const {pending} = useFormStatus();
+    //  useFormStatus should be called inside a child component of <form> 
+  return (
+<button type="submit" className="mt-2" disabled={pending}>
+        {pending ? "Submitting..." : "Submit Post"}
+      </button>
+  )
+}
+
+
+function Publishbtn(){
+    const {pending} = useFormStatus();
+  return (
+<button type="submit" className="mt-2" disabled={pending}>
+        {pending ? "Submitting..." : "Submit Post"}
+      </button>
+  )
+}
+
 
 function PostForm() {
   const [{success, error}, formAction, isPending] = useActionState(submitPost, {success : null, error: null});
+
 
 
 
@@ -40,9 +62,8 @@ function PostForm() {
         required
         name="body"
       />
-      <button type="submit" className="mt-2" disabled={isPending}>
-        {isPending ? "Submitting..." : "Submit Post"}
-      </button>
+      <SubmitFormBtn/>
+      <Publishbtn />
       {error && <p style={{ color: "red" }}>{error}</p>}
       {success && <p style={{ color: "green" }}>{success}</p>}
     </form>
